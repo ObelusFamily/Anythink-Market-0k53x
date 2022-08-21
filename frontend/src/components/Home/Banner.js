9ae9347784f889e7b12c719aeed0bdc85dd318c0
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2022-08-21 21:59:03 +0200

    Implement search filter in frontend

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,38 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = (props) => {
+  const handleInput = (ev) => {
+    ev.preventDefault();
+    const searchString = ev.target.value;
+    if (searchString.length < 3) return;
+    props.onInputSearchString(
+      searchString,
+      (page) => agent.Items.byTitle(searchString, page),
+      agent.Items.byTitle(searchString)
+    );
+  };
   return (
     <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
+      <div className="container p-4 align-items-center text-center">
+        <div className="">
+          <img src={logo} alt="banner" />
+        </div>
+        <div className="row justify-content-center align-items-center">
           <span id="get-part">A place to get</span>
+          <div className="input-group col-sm-5">
+            <input 
+              id="search-box"
+              className="form-control border-end-0"
+              type="text" 
+              placeholder="What is it that you truly desire?"
+              onInput={handleInput}
+            />
+            <span className="input-group-text bg-white ms-n3">
+              <i className="bi bi-search"></i>
+            </span>
+          </div>
           <span> the cool stuff.</span>
         </div>
       </div>
